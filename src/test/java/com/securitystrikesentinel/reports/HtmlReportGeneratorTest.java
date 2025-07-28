9533552311c4b323ac745e55c18b7aa173366c34
Toraaaamizu <rjangco.personal@gmail.com>
2025-07-29 01:46:27 +0800

    Updates to fix issues with scan

--- src/test/java/com/securitystrikesentinel/reports/HtmlReportGeneratorTest.java
+++ src/test/java/com/securitystrikesentinel/reports/HtmlReportGeneratorTest.java
@@ -2,12 +2,9 @@ package com.securitystrikesentinel.reports;
 
 import org.junit.jupiter.api.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HtmlReportGeneratorTest {
